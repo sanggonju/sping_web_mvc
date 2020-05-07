71517c89a581719537c2ba9c0ff514a09c107819
goOn <terusg90@gmail.com>
2020-05-07 17:13:19 +0900

    2강 MVC 서블릿 소개, 서블릿 애플리케이션 개발 HelloServlet

---
+++ src/main/me/goOn/HelloServlet.java
@@ -0,0 +1,37 @@
+package me.goOn;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    public void init() {
+        System.out.println("init");
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws
+            IOException {
+        System.out.println("doGet");
+        resp.getWriter().write("<html>");
+        resp.getWriter().write("<head>");
+        resp.getWriter().write("</head>");
+        resp.getWriter().write("<body>");
+        resp.getWriter().write("<h1>Hello Servlet</h1>");
+        resp.getWriter().write("</body>");
+        resp.getWriter().write("</html>");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy");
+    }
+}
+
+/*
+    init()은 최초 요청시에만 호출됨.(init-doget)
+    2번째 부터는 doGet만 호출됨.
+    서버종료시  destroy() 가 호출된다.
+* */
