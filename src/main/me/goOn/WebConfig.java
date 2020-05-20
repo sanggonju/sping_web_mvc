a95edcca103f4e9cbeccb541476ff718c1350b34
goOn <terusg90@gmail.com>
2020-05-20 10:25:49 +0900

    WebMvcConfigurer WebMvcConfigurer 인터페이스를 상속받아 설정하는 방법 configureViewResolvers, addFormatters, addInterceptors 등의 설정 가능

--- src/main/me/goOn/WebConfig.java
+++ src/main/me/goOn/WebConfig.java
@@ -4,44 +4,63 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.HandlerAdapter;
 import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.List;
 
 @Configuration
 //@ComponentScan(useDefaultFilters = false, includeFilters = @ComponentScan.Filter(Controller.class))
 @ComponentScan
 @EnableWebMvc
-public class WebConfig {
-
-    @Bean
-    public HandlerMapping handlerMapping(){
-        RequestMappingHandlerMapping handlerMapping = new RequestMappingHandlerMapping();
-        handlerMapping.setInterceptors();
-        handlerMapping.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return handlerMapping;
-    }
-
-    @Bean
-    public HandlerAdapter handlerAdapter(){
-        RequestMappingHandlerAdapter  handlerAdapter = new RequestMappingHandlerAdapter();
-        return handlerAdapter;
-    }
-
-    @Bean
+public class WebConfig implements WebMvcConfigurer {
+    /*@Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setPrefix("/WEB-INF/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
     }
+    WebMvcConfigurer를사용하면 해당 설정을 이렇게 하지 않고
+    더간단하게 사용가능
+    */
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+
+        registry.jsp("/WEB-INF/",".jsp");
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        
+    }//요청 에 따라 AceeptHeader에 jsp/ xml/ pdf/등을 설정하면 그에 맞는 것을 해서 보여줌
+
+
+
 }
 
 /*
-@EnableWebMvc
-사용시에는
+WebMvcConfigurer는 Spring에서 제공하는 것
+SpringBoot에서 제공되는 것이 아님.
 * */
